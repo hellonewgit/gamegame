ec35b8085d46055b04a7d924b8aa0e980bb15997
alexeykadochnik0v <icest00rm@gmail.com>
2025-08-11 11:40:54 +0300

    add

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,5 @@
+// Deprecated: ESLint v9 uses flat config (eslint.config.js). This file is kept empty to avoid CI picking it up.
+module.exports = {};
 // Deprecated: ESLint v9 uses eslint.config.js (flat config).
 // This file is intentionally left empty to avoid conflicts in editors.
 module.exports = {};
